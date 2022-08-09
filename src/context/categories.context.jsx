846ab94eb41a7ac94a6c6c7a29592b35a980ffd3
pnmrvvtl <pnmrv.vtl@gmail.com>
2022-08-09 21:56:53 +0300

    complete routing for categories of products

---
+++ src/context/categories.context.jsx
@@ -0,0 +1,30 @@
+
+
+import SHOP_DATA from '../shop-data.js';
+import {createContext, useEffect, useState} from "react";
+import {getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+    const value = {categoriesMap};
+
+    // useEffect(() => {
+    //     addCollectionAndDocument('categories', SHOP_DATA);
+    // },[])
+
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+        }
+        getCategoriesMap();
+    }, [])
+
+    return (
+        <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+    )
+}
---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,22 @@
+import React, {useContext} from 'react';
+import './shop.styles.scss';
+import {CategoriesContext} from "../../context/categories.context";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+
+
+const Shop = () => {
+    const {categoriesMap} = useContext(CategoriesContext);
+
+    return (
+        <div>
+            {Object.keys(categoriesMap).map(title => {
+                const products = categoriesMap[title];
+                return (
+                    <CategoryPreview key={title} title={title} products={products}/>
+                )
+            })}
+        </div>
+    );
+};
+
+export default Shop;
